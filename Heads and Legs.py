4e4d49d1e89e200f234bf62e59d235222f13a943
joseman212 <joprez28@att.net>
2020-04-25 15:18:50 -0700

    Heads and Legs

---
+++ Heads and Legs.py
@@ -0,0 +1,10 @@
+# joseman212
+# 4/25/2020
+
+
+def animals(heads, legs):
+    chickens = heads * 2 - legs / 2
+    cows = heads - chickens
+    if chickens < 0 or cows < 0 or chickens != int(chickens):
+        return "No solutions"
+    return (chickens, cows)
