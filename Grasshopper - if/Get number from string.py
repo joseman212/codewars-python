981243331e610540d7df35d3e211498aa9738ec6
joseman212 <joprez28@att.net>
2020-08-23 17:01:47 -0700

    fixed duplicates

--- Grasshopper - if/Get number from string.py
+++
@@ -1,8 +0,0 @@
-# Joseman212
-# 8/23/2020
-
-import re
-
-
-def get_number_from_string(string):
-    return int("".join(re.findall(r'\d+', string)))
--- Grasshopper - if/Grasshopper - Function syntax bebugging.py
+++
@@ -1,6 +0,0 @@
-# Joseman212
-# 8/23/2020
-
-
-def main(verb, noun):
-    return verb + noun
